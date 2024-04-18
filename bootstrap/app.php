4b8f4aac164cd2fdf907eb58917ce2347bfffae5
Oliver Brunsmann <oliver@brunsmann.io>
2024-04-18 14:02:22 +0200

    feat: added vf iframe headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\VfFrameGuard;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             '/login/iframe',
         ]);
+
+        $middleware->append(VfFrameGuard::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
