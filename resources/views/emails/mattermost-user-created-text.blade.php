a4deebffd6aa91906fcca80ee8bc1aa57b5c60bb
Oliver Brunsmann <oliver@brunsmann.io>
2026-02-03 20:17:57 +0100

    fix: update mm invite mail

--- resources/views/emails/mattermost-user-created-text.blade.php
+++ resources/views/emails/mattermost-user-created-text.blade.php
@@ -1,25 +1,27 @@
 Hallo {{ $name }},
 
-für dich wurde ein Konto im offiziellen LfV Chat eingerichtet.
-
-Die Luftfahrtvereinigung Greven e. V. betreibt einen offiziellen Chat, der im Hinblick auf den Datenschutz besondere Kriterien erfüllt. Der Serverstandort ist Deutschland.
-
+für dich wurde ein Konto im offiziellen LfV Chat eingerichtet, den die Luftfahrtvereinigung Greven e. V. mit Serverstandort in Deutschland betreibt.
 Bitte befolge die Schritte, um den Chat zu installieren.
 
--- Schritt 1: App herunterladen --
+
+++ Schritt 1: App herunterladen ++
+
 Android Download:
 https://play.google.com/store/apps/details?id=com.mattermost.rn&hl=de&gl=US
+
 iOS Download:
 https://apps.apple.com/de/app/mattermost/id1257222717
 
--- Schritt 2: Anmelden --
+
+++ Schritt 2: Anmelden ++
+
 Gib folgende Daten ein, wenn du die App installiert hast:
 
 Server-URL: chat.sportflugzentrum.de
 Anzeigename: LfV Greven
 Verbinden klicken.
 
--- Zugangsdaten --
+++ Zugangsdaten ++
 Deine persönlichen Zugangsdaten lauten:
 
 E-Mail-Adresse: {{ $email }}
