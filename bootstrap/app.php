e80ee25ad87b2cb748ce24453c4016db60e2c877
Oliver Brunsmann <oliver@brunsmann.io>
2026-02-13 15:57:56 +0100

    ci: added coolify config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,9 +12,21 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->validateCsrfTokens(except: [
-            '/vf/iframe',
-        ]);
+        $middleware
+            ->trustProxies(
+                headers: Request::HEADER_X_FORWARDED_FOR |
+                        Request::HEADER_X_FORWARDED_HOST |
+                        Request::HEADER_X_FORWARDED_PORT |
+                        Request::HEADER_X_FORWARDED_PROTO |
+                        Request::HEADER_X_FORWARDED_AWS_ELB,
+                at: [
+                    '172.16.0.0/12',
+                    '10.0.0.0/8',
+                ],
+            )
+            ->validateCsrfTokens(except: [
+                '/vf/iframe',
+            ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
