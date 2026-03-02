51b0228c0271aa5f1e5d42016d88fb96f80a9b19
Oliver Brunsmann <oliver@brunsmann.io>
2026-03-02 12:10:40 +0100

    comment out scripts

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,14 +8,14 @@ chown -R www-data:www-data storage bootstrap/cache
 
 envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-if [ "${RUN_MIGRATIONS:-false}" = "true" ]; then
-    php artisan migrate --force
-fi
-
-if [ "${APP_ENV:-production}" = "production" ]; then
-    php artisan config:cache || true
-    php artisan route:cache || true
-    php artisan view:cache || true
-fi
+# if [ "${RUN_MIGRATIONS:-false}" = "true" ]; then
+#     php artisan migrate --force
+# fi
+#
+# if [ "${APP_ENV:-production}" = "production" ]; then
+#     php artisan config:cache || true
+#     php artisan route:cache || true
+#     php artisan view:cache || true
+# fi
 
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
